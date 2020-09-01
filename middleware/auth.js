7e0c4454da07af7242f589a68089ed5cb91d6c42
Roshan <roshanbhuvad15@gmail.com>
2020-09-01 18:44:24 +0530

    JWT authentication added

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jwt-then");
+const { JWT_SECRET } = require("../key");
+const mongoose = require("mongoose");
+const Owner = mongoose.model("Owner");
+const Customer = mongoose.model("Customer");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "you must be logged in" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, JWT_SECRET, (err, payload) => {
+    if (err) {
+      return res.status(401).json({ error: "you must be logged in" });
+    }
+    const { _id } = payload;
+    Owner.findById(_id).then((userdata) => {
+      req.user = userdata;
+      const { _id } = payload;
+    Customer.findById(_id).then((customerdata) => {
+      req.customer = customerdata;
+      next();
+    });
+  });
+};
+//const mongoose = require("mongoose");
+
+/*module.exports = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) throw "Forbidden!!";
+    const token = req.headers.authorization.split(" ")[1];
+    const payload = await jwt.verify(token, JWT_SECRET);
+    req.payload = payload;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      message: "Forbidden",
+    });
+  }
+}; */
